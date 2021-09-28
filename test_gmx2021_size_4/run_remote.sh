cdc8f5b2e0e8b1b2e3275a6d3f6a4ca0b57d65db
M. Eric Irrgang <ericirrgang@gmail.com>
2021-09-28 12:28:07 +0200

    update test job scripts

--- test_gmx2021_size_4/run_remote.sh
+++ test_gmx2021_size_4/run_remote.sh
@@ -2,7 +2,7 @@ export RADICAL_LOG_LVL=DEBUG
 INPUT=/home1/02634/eirrgang/projects/lccf_gmx2021-patched/input/hiv-deer/nosugar_ver116.tpr
 PAIRS=/home1/02634/eirrgang/projects/lccf_gmx2021-patched/input/hiv-deer/pair_dist.json
 HOURS=0.5
-SIZE=1000
+SIZE=4
 python ../rp-ensemble.py \
   --workers $SIZE \
   --threads 56 \
@@ -14,7 +14,6 @@ python ../rp-ensemble.py \
   --workdir /scratch1/02634/eirrgang/randowtal/brer-rp-gmx2021-$SIZE \
   --pre ". /home1/02634/eirrgang/projects/randowtal/modules.frontera" \
   --pre "umask 007" \
-  --task /home1/02634/eirrgang/projects/lccf_rp/brer_runner.py \
+  --task /home1/02634/eirrgang/projects/randowtal/brer_runner.py \
   --python /work/02634/eirrgang/frontera/venv/randowtal/bin/python \
-  --project MCB20024 \
-  --queue large
+  --project MCB20024
